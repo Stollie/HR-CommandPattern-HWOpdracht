b2f1323e07568d2eeeb8297e0630c25cc42428cc
Stollie <remco.raaymakers@gmail.com>
2013-03-18 23:44:49 +0100

    Weg met die bestanden

---
+++ Command Pattern/Commands/MYSQLBackupCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Command_Pattern
+{
+    public class MYSQLBackupCommand : ICommand
+    {
+        private string _username, _password, _host, _databasename;
+
+        public MYSQLBackupCommand(string username, string password, string host, string databasename)
+        {
+            _username       = username;
+            _password       = password;
+            _host           = host;
+            _databasename   = databasename;
+        }
+        public void execute() 
+        {
+            Console.WriteLine("Backup van" + _databasename);
+        }
+        public void undo() { }
+    }
+}
