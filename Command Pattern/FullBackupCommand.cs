bf97ff222850cd70b55f5ff6ba6dcc26825aab31
Stollie <remco.raaymakers@gmail.com>
2013-03-18 13:49:39 +0100

    Macro command erbij.

---
+++ Command Pattern/FullBackupCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Command_Pattern
+{
+    public class FullBackupCommand : ICommand
+    {
+        private List<ICommand> commands;
+
+        void FullBackupCommand(List<ICommand> cmmnds)
+        {
+            commands = cmmnds;
+        }
+
+        public void execute()
+        {
+            foreach (ICommand command in commands)
+            {
+                command.execute();
+            }
+        }
+    }
+}
